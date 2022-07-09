7e7e12f2eac1f479731fd90184adce5272a684bf
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-09 05:22:03 +0400

    Install tailwindcss

--- backend/src/components/HelloWorld.vue
+++ backend/src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="bg-purple-500">{{ msg }}</h1>
 
   <p>
     Recommended IDE setup:
--- backend/src/main.js
+++ backend/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import './index.css';
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
