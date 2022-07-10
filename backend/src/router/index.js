edbb6b48eedc854f11ffa10ae8520277184b2ca5
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-10 09:05:17 +0400

    Create RequestPassword.vue and ResetPassword.vue forms

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Login from "../views/Login.vue";
 import Dashboard from "../views/Dashboard.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/request-password',
+    name: 'requestPassword',
+    component: RequestPassword
+  },
+  {
+    path: '/reset-password/:token',
+    name: 'resetPassword',
+    component: ResetPassword
   }
 ];
 
--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -2,13 +2,7 @@
   <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
       <div>
-        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
-          Or
-          {{ ' ' }}
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>
-        </p>
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" value="true" />
@@ -30,7 +24,7 @@
           </div>
 
           <div class="text-sm">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
+            <router-link :to="{name: 'requestPassword'}" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </router-link>
           </div>
         </div>
 
