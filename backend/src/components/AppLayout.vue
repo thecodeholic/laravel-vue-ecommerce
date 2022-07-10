a69cc137151db18295a4be9d786b7faeb0e0a0bd
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-10 09:25:11 +0400

    Create Empty Dashboard Layout

---
+++ backend/src/components/AppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        Part of the layout
+      </div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const {title} = defineProps({
+    title: String
+  })
+</script>
+
+<style scoped>
+
+</style>
--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,4 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router";
+import AppLayout from '../components/AppLayout.vue'
 import Login from "../views/Login.vue";
 import Dashboard from "../views/Dashboard.vue";
 import RequestPassword from "../views/RequestPassword.vue";
@@ -6,9 +7,16 @@ import ResetPassword from "../views/ResetPassword.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'dashboard',
-    component: Dashboard
+    path: '/app',
+    name: 'app',
+    component: AppLayout,
+    children: [
+      {
+        path: 'dashboard',
+        name: 'app.dashboard',
+        component: Dashboard
+      }
+    ]
   },
   {
     path: '/login',
