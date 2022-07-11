e4a636d6ba26f39db0ec1013529ef3c98400343b
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-11 07:39:21 +0400

    Remove console.log from router.beforeEach

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -60,7 +60,6 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  console.log(to);
   if (to.meta.requiresAuth && !store.state.user.token) {
     next({name: 'login'})
   } else if (to.meta.requiresGuest && store.state.user.token) {
