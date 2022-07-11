40dfd8cf258f09f31ec076301c7c4546e73614d1
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-11 09:08:56 +0400

    Install axios and create axiosClient

---
+++ backend/src/axios.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Zura on 12/25/2021.
+ */
+import axios from "axios";
+import store from "./store";
+import router from "./router";
+
+const axiosClient = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+})
+
+axiosClient.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${store.state.user.token}`
+  return config;
+})
+
+axiosClient.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response.status === 401) {
+    sessionStorage.removeItem('TOKEN')
+    router.push({name: 'login'})
+  }
+  throw error;
+})
+
+export default axiosClient;
