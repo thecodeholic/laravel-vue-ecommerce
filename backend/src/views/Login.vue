f32c023e2fdc9419dce9ce852c0228ec20cfea0e
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-11 09:16:52 +0400

    Show error message for incorrect credentials

--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,6 +1,28 @@
 <template>
   <GuestLayout title="Sign in to your account">
     <form class="mt-8 space-y-6" method="POST" @submit.prevent="login">
+      <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+        {{ errorMsg }}
+        <span
+          @click="errorMsg = ''"
+          class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]"
+        >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </span>
+      </div>
       <input type="hidden" name="remember" value="true"/>
       <div class="rounded-md shadow-sm -space-y-px">
         <div>
@@ -45,10 +67,14 @@
 </template>
 
 <script setup>
+import {ref} from 'vue'
 import {LockClosedIcon} from '@heroicons/vue/solid'
 import GuestLayout from "../components/GuestLayout.vue";
 import store from "../store";
 import router from "../router";
+
+let loading = ref(false);
+let errorMsg = ref("");
 
 const user = {
   email: '',
@@ -57,12 +83,15 @@ const user = {
 }
 
 function login() {
+  loading.value = true;
   store.dispatch('login', user)
     .then(() => {
+      loading.value = false;
       router.push({name: 'app.dashboard'})
     })
-    .catch(() => {
-      debugger;
+    .catch(({response}) => {
+      loading.value = false;
+      errorMsg.value = response.data.message;
     })
 }
 
