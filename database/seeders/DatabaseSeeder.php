01b0a48f8c8eab7ee0b5dd9d025c5e485415aa37
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-16 23:09:29 +0400

    Add AdminUserSeeder and ProductSeeder in DatabaseSeeder.php class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            AdminUserSeeder::class,
+            ProductSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
