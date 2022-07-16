eb4046fb4f685a20baf39fd0e6d82a8502427d81
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-07-16 23:06:25 +0400

    Install `spatie/laravel-sluggable` package and implement slug generation in Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
     use HasFactory;
+    use HasSlug;
+    use SoftDeletes;
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
