b45f8d36e375c112e59ac85040c92bdd3c7be9f3
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-08-06 11:00:34 +0400

    Set $wrap = false on ProductResource.php

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\URL;
 
 class ProductResource extends JsonResource
 {
+    public static $wrap = false;
+
     /**
      * Transform the resource into an array.
      *
