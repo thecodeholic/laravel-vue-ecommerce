79b52d7166e6a1054376aacd198975d88d4a20ff
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-08-13 05:59:48 +0400

    Fix submit button position in ProductModal

--- backend/src/views/Products/ProductModal.vue
+++ backend/src/views/Products/ProductModal.vue
@@ -51,7 +51,8 @@
                 </div>
                 <footer class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button type="submit"
-                          class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-3">
+                          class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm
+                          text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500">
                     Submit
                   </button>
                   <button type="button"
