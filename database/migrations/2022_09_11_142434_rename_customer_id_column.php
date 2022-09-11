98899ac4ef61c357434a245e106145c19d052e34
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-09-11 18:29:44 +0400

    Create migration to rename customer table id column into user_id
    Install `doctrine/dbal` also

---
+++ database/migrations/2022_09_11_142434_rename_customer_id_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('customers', function (Blueprint $table) {
+             $table->renameColumn('id', 'user_id');
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+             $table->renameColumn('user_id', 'id');
+         });
+    }
+};
