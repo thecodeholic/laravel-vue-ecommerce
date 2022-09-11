bbf138dceb793229642dcf590d3915649c7f5017
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-09-11 13:48:26 +0400

    Create reusable method `Cart::getCookieCartItems`

--- app/Helpers/Cart.php
+++ app/Helpers/Cart.php
@@ -45,8 +45,14 @@ class Cart
                 fn($item) => ['product_id' => $item->product_id, 'quantity' => $item->quantity]
             );
         } else {
-            return json_decode($request->cookie('cart_items', '[]'), true);
+            return self::getCookieCartItems();
         }
+    }
+
+    public static function getCookieCartItems()
+    {
+        $request = \request();
+        return json_decode($request->cookie('cart_items', '[]'), true);
     }
 
     public static function getCountFromItems($cartItems)
@@ -56,5 +62,27 @@ class Cart
             fn($carry, $item) => $carry + $item['quantity'],
             0
         );
+    }
+
+    public static function moveCartItemsIntoDb()
+    {
+        $request = \request();
+        $cartItems = self::getCookieCartItems();
+        $dbCartItems = CartItem::where(['user_id' => $request->user()->id])->get()->keyBy('product_id');
+        $newCartItems = [];
+        foreach ($cartItems as $cartItem) {
+            if (isset($dbCartItems[$cartItem['product_id']])) {
+                continue;
+            }
+            $newCartItems[] = [
+                'user_id' => $request->user()->id,
+                'product_id' => $cartItem['product_id'],
+                'quantity' => $cartItem['quantity'],
+            ];
+        }
+
+        if (!empty($newCartItems)) {
+            CartItem::insert($newCartItems);
+        }
     }
 }
