a8b6a1ff98bc545299a9b41ed1c21dc236b686ae
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-09-17 07:36:38 +0400

    Create ProfileController with view method

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\AddressType;
+use App\Http\Requests\ProfileRequest;
+use App\Models\Country;
+use App\Models\CustomerAddress;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function view(Request $request)
+    {
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+        /** @var \App\Models\Customer $customer */
+        $customer = $user->customer;
+        $shippingAddress = $customer->shippingAddress ?: new CustomerAddress(['type' => AddressType::Shipping]);
+        $billingAddress = $customer->billingAddress ?: new CustomerAddress(['type' => AddressType::Billing]);
+        $countries = Country::query()->orderBy('name')->get();
+        return view('profile.view', compact('customer', 'user', 'shippingAddress', 'billingAddress', 'countries'));
+    }
+}
