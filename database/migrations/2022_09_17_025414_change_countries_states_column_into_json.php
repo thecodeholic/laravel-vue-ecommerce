5c4278ec70fb4181b9c1e38c3a0606a2c7a06955
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-09-17 07:24:52 +0400

    Change countries table states column into json field

---
+++ database/migrations/2022_09_17_025414_change_countries_states_column_into_json.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('countries', function (Blueprint $table) {
+            $table->dropColumn('states');
+        });
+        Schema::table('countries', function (Blueprint $table) {
+            $table->json('states')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('countries', function (Blueprint $table) {
+            $table->longText('states')->nullable()->change();
+        });
+    }
+};
