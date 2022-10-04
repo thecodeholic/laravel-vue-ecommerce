6540f7dd6672f4132533c196bde0685683cb0ce1
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-10-04 10:33:12 +0400

    Add relations to Order and OrderItem models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,12 +2,30 @@
 
 namespace App\Models;
 
+use App\Enums\OrderStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
     use HasFactory;
 
     protected $fillable = ['status', 'total_price', 'created_by', 'updated_by'];
+
+    public function isPaid()
+    {
+        return $this->status === OrderStatus::Paid->value;
+    }
+
+    public function payment(): HasOne
+    {
+        return $this->hasOne(Payment::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_id', 'unit_price', 'product_id', 'quantity'];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
