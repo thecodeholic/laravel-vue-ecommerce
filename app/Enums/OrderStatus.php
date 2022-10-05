d2f27d043bbea198d3b7a0c0e001dbea4ef8cbf9
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-10-05 10:46:04 +0400

    Add few more statuses in OrderStatus enum

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -18,5 +18,14 @@ enum OrderStatus: string
 {
     case Unpaid = 'unpaid';
     case Paid = 'paid';
-    case Completed = 'complete';
+    case Cancelled = 'cancelled';
+    case Shipped = 'shipped';
+    case Completed = 'completed';
+
+    public static function getStatuses()
+    {
+        return [
+            self::Paid, self::Unpaid, self::Cancelled, self::Shipped, self::Completed
+        ];
+    }
 }
