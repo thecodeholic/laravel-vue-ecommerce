68aada3d793ffb37ef2f40aacc546a8b32cebb1c
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-10-11 20:01:25 +0400

    Fix the width of vuejs sidebar

--- backend/src/components/Sidebar.vue
+++ backend/src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-[200px]  transition-all bg-indigo-700 text-white py-4 px-2">
+  <div class="min-w-[160px] w-[160px]  transition-all bg-indigo-700 text-white py-4 px-2">
     <router-link :to="{name: 'app.dashboard'}"
                  class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
       <span class="mr-2 text-gray-300">
