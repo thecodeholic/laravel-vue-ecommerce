be94b324e10eb6a3c4881532a7b0f447ac80f2de
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2023-09-02 06:58:12 +0400

    Return image_url on frontend from product_images table

--- app/Http/Resources/ProductListResource.php
+++ app/Http/Resources/ProductListResource.php
@@ -11,7 +11,7 @@ class ProductListResource extends JsonResource
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
@@ -19,10 +19,10 @@ class ProductListResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'image_url' => $this->image ?: null,
+            'image_url' => !empty($this->images) ? $this->images[0]->url : null,
             'price' => $this->price,
             'quantity' => $this->quantity,
-            'updated_at' => (new \DateTime($this->updated_at))->format('Y-m-d H:i:s'),
+            'updated_at' => ( new \DateTime($this->updated_at) )->format('Y-m-d H:i:s'),
         ];
     }
 }
