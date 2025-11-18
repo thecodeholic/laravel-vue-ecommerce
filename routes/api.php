dc771c7a17f089fb78f20ded81eb6c41fc95ba93
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2025-11-18 12:08:32 +0400

    Remove RouteServiceProvider usage

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
